76f19cb9c5e5a8050b3acfcf4c8ad13f6c12f38e
Aaron Bassett <me@aaronbassett.com>
2013-01-29 21:11:54 +0000

    Package up

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='sometimes',
+    version='0.1.1',
+    author='Aaron Bassett',
+    author_email='me@aaronbassett.com',
+    packages=['sometimes',],
+    url='https://github.com/aaronbassett/sometimes',
+    license='LICENSE',
+    description='Stop being so black and white. Mix things up a bit and execute code sometimes.',
+    long_description=open('README.rst').read(),
+)
